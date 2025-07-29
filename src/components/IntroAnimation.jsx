0d1a398132213c14b44691706c14fe95bd401a8d
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-07-29 22:46:22 +0300

    add design in this branch 2.2

--- src/components/IntroAnimation.jsx
+++ src/components/IntroAnimation.jsx
@@ -40,22 +40,22 @@ const IntroAnimation = () => {
 
       <div className="w-[50%] h-full bg-yellow-200 flex justify-center items-center">
         <div className="w-[75%] h-[70%] flex flex-col gap-2 ">
-          <div className="w-full h-[80%] bg-yellow-50 rounded-2xl p-5 py-7">
-           <div className="w-full h-[25%] bg-blue-500 p-2 flex flex-row gap-2 justify-start items-center">
-              <div className="w-20 h-20 rounded-full bg-amber-50"></div>
-              <div className="w-96 h-20 bg-amber-600"></div>
-           </div>
+          <div className="w-full h-[80%] p-5 py-7 flex justify-center items-center">
+            {/* <div className="w-70 h-70 rounded-full bg-yellow-50 flex justify-center items-center"> */}
+               <img src="/src/assets/images/Pok-dex-3-4-2025 1.svg" alt="" /> 
+            {/* </div> */}
+         
           </div>
           <div className="w-full h-[20%] flex flex-col justify-center items-center ">
             <button
               id="start-btn"
               class="px-8 py-3 bg-yellow-300 hover:bg-yellow-100 rounded-full font-bold tracking-wider transform hover:scale-105 transition-all duration-300 shadow-lg shadow-red-900/50"
             >
-              DÉMARRER
+             Go Start
             </button>
 
             <div class="mt-8 text-xm text-black animate-pulse">
-              Appuyez sur Démarrer pour commencer votre aventure Pokémon!
+              Press Start key to begin your Pokemon Adventure !
             </div>
           </div>
         </div>
