8b2956b6929aa117d57cecda7bb438971fe5769b
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-07-30 16:25:44 +0300

    strucured home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen h-screen bg-[var(--fond-principal)]'> 
-      Hello
+    <div className='w-screen h-screen bg-yellow-200 flex flex-col'> 
+      <nav className='w-full h-[10%] bg-[var(--color-header)] flex justify-center items-center text-3xl'>
+      Searchbar.jsx
+      </nav>
+     <main className='w-full h-[90%] bg-amber-700 flex md:flex-row  flex-col'>
+      <div className='w-[70%] h-full bg-blue-800 hidden md:flex justify-center items-center text-3xl'>PokemonCard.jsx</div>
+      <div className='md:w-[30%] h-full w-full bg-cyan-500 flex justify-center items-center text-3xl'>PokemonDetails.jsx</div>
+     </main>
     </div>
   )
 }
