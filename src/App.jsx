d2eeb617d640ecc585851811df127212844bf0eb
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-07-30 11:54:47 +0300

    configure start key

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IntroAnimation from './components/IntroAnimation';
 import Home from './pages/Home';
 
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setShowIntro(false), 300000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <Router>
-      {showIntro ? (
-        <IntroAnimation onComplete={() => setShowIntro(false)} />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Loading/>} />
-        </Routes>
-
-      )}
+      <Routes>
+        <Route path="/" element={<IntroAnimation />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
