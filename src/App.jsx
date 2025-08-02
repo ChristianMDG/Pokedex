209447269dbd0dca64e09b799daa98725f1dd99c
valeria <hei.valeria.2@gmail.com>
2025-08-02 10:18:11 +0300

    resolve bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IntroAnimation from './components/IntroAnimation';
-import Home from './pages/Home';
+import Home from './pages/Home'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<IntroAnimation />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<IntroAnimation/>} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </Router>
   );
