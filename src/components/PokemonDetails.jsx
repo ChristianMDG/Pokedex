3813f9baab83fabd45a85da4093f01bda2b944b7
ChristianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-08-02 08:42:11 +0300

    adapt image

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const PokemonDetails = ({ pokemon }) => {
   const getWeaknesses = (types) => {
     const typeChart = {
@@ -5,18 +7,28 @@ const PokemonDetails = ({ pokemon }) => {
       fire: ['water', 'rock', 'ground'],
       water: ['electric', 'grass'],
       grass: ['fire', 'ice', 'poison', 'flying', 'bug'],
-     
+      // Ajoute d'autres types ici si nécessaire
     };
     const weaknesses = new Set();
-    types.forEach(type => typeChart[type.type.name]?.forEach(w => weaknesses.add(w)));
+    types.forEach(type =>
+      typeChart[type.type.name]?.forEach(w => weaknesses.add(w))
+    );
     return Array.from(weaknesses);
   };
 
+  const getIdFromUrl = (url) => url.split('/').filter(Boolean).pop();
+
   return (
-    <div className="bg-pokedex-red p-4 rounded-lg mt-4">
-      <h2 className="text-xl font-bold capitalize">{pokemon.name}</h2>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} className="mx-auto mt-2" />
-      <div className="mt-2">
+    <div className="bg-pokedex-red p-4 rounded-lg mt-4 text-white">
+      <h2 className="text-xl font-bold capitalize text-center">{pokemon.name}</h2>
+      
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+        alt={pokemon.name}
+        className="mx-auto mt-2 w-40 h-40 object-contain"
+      />
+
+      <div className="mt-4 space-y-1">
         <p><span className="font-bold">Height:</span> {pokemon.height / 10} m</p>
         <p><span className="font-bold">Weight:</span> {pokemon.weight / 10} kg</p>
         <p><span className="font-bold">Health:</span> {pokemon.stats[0].base_stat} HP</p>
@@ -24,15 +36,21 @@ const PokemonDetails = ({ pokemon }) => {
         <p><span className="font-bold">Abilities:</span> {pokemon.abilities.map(a => a.ability.name).join(', ')}</p>
         <p><span className="font-bold">Weakness:</span> {getWeaknesses(pokemon.types).join(', ')}</p>
       </div>
-      <div className="mt-2">
+
+      <div className="mt-4">
         <h3 className="font-bold">Evolution:</h3>
-        <div className="flex space-x-2">
-          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} className="w-16 h-16" />
-          {pokemon.evolution.evolves_to.length > 0 && (
+        <div className="flex items-center space-x-2 mt-2">
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`}
+            alt={pokemon.name}
+            className="w-16 h-16"
+          />
+
+          {pokemon.evolution?.evolves_to?.length > 0 && (
             <>
               <p>→</p>
               <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.evolution.evolves_to[0].species.url.split('/').slice(-2, -1)[0]}.png`}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${getIdFromUrl(pokemon.evolution.evolves_to[0].species.url)}.png`}
                 alt={pokemon.evolution.evolves_to[0].species.name}
                 className="w-16 h-16"
               />
@@ -40,7 +58,7 @@ const PokemonDetails = ({ pokemon }) => {
                 <>
                   <p>→</p>
                   <img
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.evolution.evolves_to[0].evolves_to[0].species.url.split('/').slice(-2, -1)[0]}.png`}
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${getIdFromUrl(pokemon.evolution.evolves_to[0].evolves_to[0].species.url)}.png`}
                     alt={pokemon.evolution.evolves_to[0].evolves_to[0].species.name}
                     className="w-16 h-16"
                   />
@@ -50,12 +68,14 @@ const PokemonDetails = ({ pokemon }) => {
           )}
         </div>
       </div>
-      <div className="mt-2">
-        <p className="italic">{pokemon.description.replace('\f', ' ')}</p>
+
+      <div className="mt-4 italic">
+        {pokemon.description && <p>{pokemon.description.replace('\f', ' ')}</p>}
       </div>
-      <div className="flex justify-between mt-4">
-        <button className="bg-gray-300 p-2 rounded">{'< 1025'}</button>
-        <button className="bg-gray-300 p-2 rounded">{'0002 >'}</button>
+
+      <div className="flex justify-between mt-6">
+        <button className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400">{'< Précédent'}</button>
+        <button className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400">{'Suivant >'}</button>
       </div>
     </div>
   );
